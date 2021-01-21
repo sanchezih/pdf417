7524e3074c30ada56d3388cd31dddb5b207c2f75
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2021-01-21 12:48:23 -0300

    reorganizo codigo

--- src/main/java/com/init/barcode/rest/BarcodeREST.java
+++ src/main/java/com/init/barcode/rest/BarcodeREST.java
@@ -2,34 +2,21 @@ package com.init.barcode.rest;
 
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.imageio.ImageIO;
-
-import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
 import com.google.zxing.BinaryBitmap;
-import com.google.zxing.EncodeHintType;
 import com.google.zxing.LuminanceSource;
 import com.google.zxing.MultiFormatReader;
 import com.google.zxing.NotFoundException;
 import com.google.zxing.Result;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.common.HybridBinarizer;
-import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import com.init.barcode.entity.Barcode;
 
 @RestController
@@ -39,29 +26,29 @@ public class BarcodeREST {
 //	@Autowired
 //	private BarcodeDAO BarcodeDAO;
 
-	@RequestMapping(value = "pdf4172", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Barcode> decodedBarcode2(@RequestBody String encodedImage) throws Exception {
+	@RequestMapping(value = "pdf417", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Barcode> decodedBarcode(@RequestBody String encodedImage) throws Exception {
 
 		Barcode bc = new Barcode();
 		bc.setData(null);
 
 		try {
-			String decodedText = decodeQRCode(encodedImage);
+			String decodedText = decodeCode(encodedImage);
 			if (decodedText == null) {
-				System.out.println("No QR Code found in the image");
+				System.out.println("No PDF417 Code found in the image");
+				return ResponseEntity.badRequest().build();
 			} else {
 				System.out.println("Decoded text = " + decodedText);
 				bc.setData(decodedText);
 			}
 		} catch (IOException e) {
-			System.out.println("Could not decode QR Code, IOException :: " + e.getMessage());
+			System.out.println("Could not decode PDF417 Code, IOException :: " + e.getMessage());
 		}
-
 		return ResponseEntity.ok(bc);
 
 	}
 
-	private static String decodeQRCode(String data) throws IOException {
+	private static String decodeCode(String data) throws IOException {
 
 		String base64Image = data.split(",")[1];
 		byte[] imageBytes = javax.xml.bind.DatatypeConverter.parseBase64Binary(base64Image);
@@ -73,7 +60,7 @@ public class BarcodeREST {
 			Result result = new MultiFormatReader().decode(bitmap);
 			return result.getText();
 		} catch (NotFoundException e) {
-			System.out.println("There is no QR code in the image");
+			System.out.println("There is no PDF417 code in the image");
 			return null;
 		}
 	}
